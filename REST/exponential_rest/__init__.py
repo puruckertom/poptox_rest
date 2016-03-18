172f5aeaa4bcd071bf6c95c1d174056c1ce0e717
Jon Flash <funkswing@gmail.com>
2016-03-18 14:35:34 -0400

    Updated Stubbed PopTox REST

---
+++ REST/exponential_rest/__init__.py
@@ -0,0 +1,53 @@
+from flask_restful import Resource
+from poptox.poptox.exponential import exponential_exe as exponential
+from flask import request
+from REST import rest_validation, rest_schema, rest_model_caller
+
+
+class ExponentialHandler(Resource):
+    def __init__(self):
+        self.name = "exponential"
+
+    @staticmethod
+    def get_model_inputs():
+        """
+        Return model's input class.
+        :return:
+        """
+        return exponential.ExponentialInputs()
+
+    @staticmethod
+    def get_model_outputs():
+        """
+        Return model's output class.
+        :return:
+        """
+        return exponential.ExponentialOutputs()
+
+
+class ExponentialGet(ExponentialHandler):
+
+    def get(self, jobId="YYYYMMDDHHMMSSuuuuuu"):
+        """
+        Exponential get handler.
+        :param jobId: (format = %Y%m%d%H%M%S%f) (15 digits)
+        :return:
+        """
+        return rest_schema.get_schema(self.name, jobId)
+
+
+class ExponentialPost(ExponentialHandler):
+
+    def post(self, jobId="000000100000011"):
+        """
+        Exponential post handler.
+        :param jobId:
+        :return:
+        """
+
+        inputs = rest_validation.parse_inputs(request.json)
+
+        if inputs:
+            return rest_model_caller.model_run(self.name, jobId, inputs, module=exponential)
+        else:
+            return rest_model_caller.error(self.name, jobId, inputs)
---
+++ wsgi.py
@@ -0,0 +1,16 @@
+import bottle, os, sys
+
+# Change working directory so relative paths (and template lookup) work again
+os.chdir(os.path.dirname(__file__))
+
+# Add Django project & parent directory to Python PATH
+sys.path.insert(0, '/var/www/ubertool/ubertool_ecorest/REST_UBER')
+sys.path.insert(0, '/var/www/ubertool/ubertool_ecorest')
+# sys.path.insert(0, '/var/www/ubertool')
+
+# ... build or import your bottle application here ...
+import poptox_flask
+
+# Do NOT use bottle.run() with mod_wsgi/uwsgi
+
+application = bottle.default_app()
